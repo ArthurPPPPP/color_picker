e60fd4398844e0beb8bdf6cbef3586aeb8ce62f8
ArthurPPPPP <92211379+ArthurPPPPP@users.noreply.github.com>
2021-12-17 15:55:02 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,8 @@ import React from "react";
 import "./styles.scss";
 import { Link } from "react-router-dom";
 export const Header = () => {
-  const styles = {
-    background: `rgb(255, 255, 255)`,
-    background: `linear-gradient(
-    0deg,
-    rgba(255, 255, 255, 1) 32%,
-    rgba(205, 205, 205, 1) 120%
-  )`,
-  };
   return (
-    <header className="header" style={styles}>
+    <header className="header">
       <Link to={"/"} className="link">
         <h1>Color Picker</h1>
       </Link>
